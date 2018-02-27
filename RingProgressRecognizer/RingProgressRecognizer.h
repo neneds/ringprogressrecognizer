5a50b8ec473177498a23f4b739e54665d8182986
DennisMerli <dennis.rodrigues@aisdigital.com.br>
2018-02-27 16:03:30 -0300

    Initial Commit

---
+++ RingProgressRecognizer/RingProgressRecognizer.h
@@ -0,0 +1,19 @@
+//
+//  RingProgressRecognizer.h
+//  RingProgressRecognizer
+//
+//  Created by Dennis Merli on 27/02/18.
+//  Copyright © 2018 Dennis Merli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RingProgressRecognizer.
+FOUNDATION_EXPORT double RingProgressRecognizerVersionNumber;
+
+//! Project version string for RingProgressRecognizer.
+FOUNDATION_EXPORT const unsigned char RingProgressRecognizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RingProgressRecognizer/PublicHeader.h>
+
+
